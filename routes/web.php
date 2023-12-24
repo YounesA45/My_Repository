799b6b277dad8eb6469f2979c713388f44140829
YounesA45 <98330934+Younes0Yassine@users.noreply.github.com>
2023-12-24 11:56:48 +0100

    add Auth package

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/tableDaccreditation', [PostAccreditationController::class, 'showPost'])->name('showPost');
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
